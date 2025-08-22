df158908ccc98efdea6bc84ebcdd516b0444d0e4
Franz TRIERWEILER <trierweilerfranz@gmail.com>
2025-08-22 17:31:32 +0200

    Simplification du script

--- install/install.sh
+++ install/install.sh
@@ -9,6 +9,7 @@ PYTHON="python3"
 sudo apt-get update
 sudo apt-get install $PYTHON
 sudo apt-get install -y python3.10-venv
+sudo apt-get install pip
 
 # Vérifier si l'environnement virtuel existe déjà
 if [ -d "$VENV_NAME" ]; then
@@ -23,40 +24,8 @@ else
     echo "Environnement virtuel '$VENV_NAME' créé avec succès."
 fi
 
-# Vérifier si pip est installé
-if ! command -v pip &> /dev/null; then
-    echo "pip n'est pas installé dans l'environnement virtuel. Installation en cours via apt-get..."
-    if command -v apt-get &> /dev/null; then
-        sudo apt-get update
-        sudo apt-get install -y python3-pip
-        if [ $? -ne 0 ]; then
-            echo "Erreur : Impossible d'installer python3-pip via apt-get."
-            exit 1
-        fi
-        # S'assurer que pip est disponible dans l'environnement virtuel
-        $PYTHON -m ensurepip --upgrade
-        $PYTHON -m pip install --upgrade pip
-        echo "pip installé avec succès."
-    else
-        echo "Erreur : Gestionnaire de paquets 'apt-get' non trouvé. Veuillez installer pip manuellement."
-        exit 1
-    fi
-else
-    echo "pip est déjà installé."
-fi
-
 # Activer l'environnement virtuel
 source $VENV_NAME/bin/activate
 
-# Vérifier si mistralai est installé
-if pip show mistralai &> /dev/null; then
-    echo "La bibliothèque 'mistralai' est déjà installée."
-else
-    echo "Installation de la bibliothèque 'mistralai'..."
-    pip install mistralai
-    if [ $? -ne 0 ]; then
-        echo "Erreur lors de l'installation de 'mistralai'."
-        exit 1
-    fi
-    echo "Bibliothèque 'mistralai' installée avec succès."
-fi
+# Installer dans l'environnement virtuel les différents paquetages Pyhthon
+pip install mistralai
